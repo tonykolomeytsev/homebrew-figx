9f335b5b83dd548d688b7d8a2742f4578b24f3dd
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2025-05-19 14:01:30 +0400

    v0.2.4

--- Formula/figmagic.rb
+++ Formula/figmagic.rb
@@ -4,9 +4,9 @@
 class Figmagic < Formula
   desc "Cross-platform Figma resources management CLI tool"
   homepage "https://github.com/tonykolomeytsev/figmagic"
-  url "https://github.com/tonykolomeytsev/figmagic/releases/download/v0.2.3/figmagic-macOS-arm64.tar.gz"
-  sha256 "ce56ef7e7806c9df8d68b30a1f2f47fc25fa5f863288d70ee0a6ce571d1e40c7"
-  version "0.2.3"
+  url "https://github.com/tonykolomeytsev/figmagic/releases/download/v0.2.4/figmagic-macOS-arm64.tar.gz"
+  sha256 "5934599032140c6cc5aa2f4204a52421618ce3f5440b19d91ecac76970dd05a1"
+  version "0.2.4"
 
   def install
     bin.install "figmagic"
