c1dce477d52632322c8c2ac0a1ff1a67604ae81e
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2025-06-06 12:03:46 +0400

    Update figx.rb

--- Formula/figx.rb
+++ Formula/figx.rb
@@ -5,7 +5,7 @@ class Figx < Formula
   desc "Cross-platform Figma resources management CLI tool"
   homepage "https://github.com/tonykolomeytsev/figx"
   url "https://github.com/tonykolomeytsev/figx/releases/download/v0.4.2/figx-macOS-arm64.tar.gz"
-  sha256 "sha256:6aea576fa1be9836bcf0993a9c4f6ba3bae008028a00f79499b347adea88e5ad"
+  sha256 "6aea576fa1be9836bcf0993a9c4f6ba3bae008028a00f79499b347adea88e5ad"
   version "0.4.2"
 
   def install
